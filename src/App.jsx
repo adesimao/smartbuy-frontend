201689ed22bc2e39d0c97a7f7eb80bd80cd4cb2d
André Simão <andresimao353@gmail.com>
2025-06-03 12:41:46 +0100

    feat(App, Home, GlobalStyle): creation of the homepage and configuration of the global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { GlobalStyle } from "./styles/GlobalStyle";
+
+import Home from "./pages/Home";
+
 function App() {
   return (
     <>
-      <h1>SmartBuy - Sua Melhor Escolha!</h1>
+      <GlobalStyle />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </>
   );
 }
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+         <div>
+            <h1>Bem-vindo ao SmartBuy 👋</h1>
+            <p>Sistema de gestão de compras pessoais</p>
+         </div>
+    )
+}
+
+export default Home
